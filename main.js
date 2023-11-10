a934a4959f26e6256675f8dd70dd574c42a4cfe9
Rebecca Hansson <rebeccahansson2016@gmail.com>
2023-11-10 09:28:49 +0100

    cleaned up main.js

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-//Fixa ny fontfamily. 
-//Städa kod 
-
-//Kolla på error hantering igen och lägg till mer? 
-
 const apiKey = "72c683fa486d6d0335603532705a98ff";
-const baseUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}`
+const baseUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}`;
 
 // variabel for the toggle-function.
 let isCelsius = true; 
@@ -12,7 +7,6 @@ let isCelsius = true;
 const h2 = document.querySelector('.h2');
 const h1 = document.querySelector('.h1');
 const firstH3 = document.querySelector('.h3-1');
-const secondH3 = document.querySelector('.h3-2'); //Används ej just nu
 const icon = document.querySelector('.icon');
 const wikiHeader = document.querySelector('.wikipedia-header');
 const highLow = document.querySelector('.high-low');
@@ -22,24 +16,22 @@ function getUnit() {
     return isCelsius ? 'metric' : 'imperial';
 }
 
-
-//Checkweather borde kasta error. PrintError()
 //Function connected to the search-btn. 
 async function checkWeather() {
     try {
         h1.innerHTML = "";
         h2.innerHTML = "";
         icon.style.display = 'none'; 
-        document.querySelector('.wikipedia-content').innerHTML="";
+        document.querySelector('.wikipedia-content').innerHTML = "";
 
         const inputCity = document.getElementById('search-bar').value;
         //Calling getUnit in the url to get the accurate unitvalue. 
-        const weatherUrl = `${baseUrl}&units=${getUnit()}&q=${inputCity}`
+        const weatherUrl = `${baseUrl}&units=${getUnit()}&q=${inputCity}`;
         const fetchedData = await fetchData(weatherUrl);
 
         //If fetch went through - call the wikipedia-fetch. 
         if (fetchedData) {
-            const wikipediaSummary = await fetchData(`https://sv.wikipedia.org/api/rest_v1/page/summary/${inputCity}`)
+            const wikipediaSummary = await fetchData(`https://sv.wikipedia.org/api/rest_v1/page/summary/${inputCity}`);
             printResult(fetchedData);
             printSummary(wikipediaSummary);
         }
@@ -60,11 +52,11 @@ async function fetchData(url) {
         return data;
     } else {
         printError();
-        // console.error("API request failed:", response.status);
-        // return null; 
-        throw new Error(`API request failed with status ${response.status}`);
+         console.error("API request failed:", response.status);
+         return null; 
     }
 }
+
 //Function for printing results. 
 const printResult = (dataInput) => {
     document.querySelector('.error').innerHTML = "";
@@ -82,7 +74,7 @@ const printResult = (dataInput) => {
         h1.innerHTML = `${temperature}${unit}`;
         highLow.innerHTML = `Högsta: ${Math.round(dataInput.main.temp_max)}${unit} <br>Lägsta: ${Math.round(dataInput.main.temp_min)}${unit}`;
 
-        //Make the borders visable. 
+        //Make the borders visable (depending on screen-size).
         document.querySelector('.box1').style.borderRight = '2px solid rgb(218, 218, 218)';
         document.querySelector('.box2').style.borderRight = '2px solid rgb(218, 218, 218)';
         if (window.innerWidth <= 820) {
@@ -95,42 +87,44 @@ const printResult = (dataInput) => {
 
         //Adding icons depending on the description
         const weatherIcon = document.querySelector('.icon');
-        if (dataInput.weather[0].description.includes('snow')){
+        if (dataInput.weather[0].description.includes('snow')) {
             weatherIcon.src = "IMG/snow.png";
         }
-        else if (dataInput.weather[0].description.includes('rain')||
-                dataInput.weather[0].description.includes('drizzle')){
+        else if (dataInput.weather[0].description.includes('rain') ||
+                dataInput.weather[0].description.includes('drizzle')) {
                 weatherIcon.src = "IMG/rain.png";
         }
-        else if (dataInput.weather[0].description.includes('cloud')){
+        else if (dataInput.weather[0].description.includes('cloud')) {
                 weatherIcon.src = "IMG/clouds.png";
         }
-        else if (dataInput.weather[0].description.includes('fog')||
-                dataInput.weather[0].description.includes('mist')){
+        else if (dataInput.weather[0].description.includes('fog') ||
+                dataInput.weather[0].description.includes('mist')) {
                 weatherIcon.src = "IMG/clouds.png";
         }
-        else if (dataInput.weather[0].description.includes('clear')){
+        else if (dataInput.weather[0].description.includes('clear')) {
                 weatherIcon.src = "IMG/sun.png";
         }
         weatherIcon.style.display = 'block';
     }
 }
-//Function for switching between celsius and fahrenheit.
-/* try-catch här elleh? */
-async function changeMetric() {
 
+//Function for switching between celsius and fahrenheit.
+async function changeMetric() {
+    try {
     const imgElement = document.querySelector('.toggle-icon');
     imgElement.src = isCelsius ? "IMG/toggle_off.png" : "IMG/toggle_on.png";
-    
+
     isCelsius = !isCelsius; // Toggle the value true/false
 
     const inputCity = document.getElementById('search-bar').value;
+    const weatherUrl = `${baseUrl}&units=${getUnit()}&q=${inputCity}`;
+    const fetchedData = await fetchData(weatherUrl);
+    printResult(fetchedData);
 
-        const weatherUrl = `${baseUrl}&units=${getUnit()}&q=${inputCity}`;
-        const fetchedData = await fetchData(weatherUrl);
-        printResult(fetchedData);
-
-
+    } catch(error) {
+        printError();
+        console.log('An error has occured.', error);  
+    }
 }
 
 //Function for printing out wikipedia-fetch
@@ -142,9 +136,7 @@ function printSummary(wikipediaSummary) {
  }
 
  function printError() {
-
-    //Skriva om det här med removechild? 
-    document.querySelector('.error').innerHTML = "Kunde inte hitta det du sökte efter. Försök igen.";
+    document.querySelector('.error').innerHTML = 'Kunde inte hitta det du sökte efter. Försök igen.';
     document.querySelector('.error').style.display = 'block';
     document.querySelector('.box1').style.borderRight = '2px solid transparent';
     document.querySelector('.box2').style.borderRight = '2px solid transparent';
